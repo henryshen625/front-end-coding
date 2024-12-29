9b0e97e9a7f0cfc3aa51eadd79074e9c38faf8c9
HenryS <henryshen625@gmail.com>
2024-12-29 14:41:31 -0600

    more changes

--- test.js
+++ test.js
@@ -56,14 +56,16 @@ function throttle(func, wait) {
 }
 
 // deeplone 简易版
-function sampleDeepClone(value) {
+function deepClone(value) {
     if (typeof value !== 'object' || value === null) {
-        return null;
+        return value;
     }
+
     if (Array.isArray(value)) {
-        return value.map(item => sampleDeepClone(item));
+        return value.map((item) => deepClone(item));
     }
-    return Object.fromEntries(Object.entries(value).map(([key, val]) => [key, sampleDeepClone(value)]));
+
+    return Object.fromEntries(Object.entries(value).map(([key, val]) => [key, deepClone(val)]));
 }
 
 //loadash._Get
@@ -166,4 +168,120 @@ function serializeHTML(element, indent = '\t') {
     return traverse(element, 0);
 }
 
+// event Emitter2 
+class EventEmitter {
+    constructor() {
+        this._events = Object.create(null);
+        this._key = 0;
+    }
 
+    on(eventName, listener) {
+        if (!Object.hasOwn(this._events, eventName)) {
+            this._events[eventName] = {};
+        }
+        const listenerId = this._key;
+        this._events[eventName][listenerId] = listener;
+        this._key++;
+        return {
+            off: () => {
+                delete this._events[eventName][listenerId];
+            }
+        };
+    }
+    
+      /**
+       * @param {string} eventName
+       * @param {...any} args
+       * @returns boolean
+       */
+    emit(eventName, ...args) {
+        if (!Object.hasOwn(this._events, eventName) || Object.keys(this._events[eventName]).length === 0) {
+            return false;
+        }
+        const listeners = { ...this._events[eventName] };
+        Object.values(listeners).forEach((listener) => {
+            listener.apply(null, args);
+        })
+        return true;
+    }
+
+}
+
+// data selection 
+
+function setHasOverlap(setA, setB) {
+    // Bundler doesn't transpile properly when doing for-of for sets.
+    for (const val of Array.from(setA)) {
+      if (setB.has(val)) {
+        return true;
+      }
+    }
+  
+    return false;
+  }
+  
+  /**
+   * @param {Array<{user: number, duration: number, equipment: Array<string>}>} sessions
+   * @param {{user?: number, minDuration?: number, equipment?: Array<string>, merge?: boolean}} [options]
+   * @return {Array}
+   */
+  export default function selectData(sessions, options = {}) {
+    const reversedSessions = sessions.slice().reverse(); // Make a copy and reverse.
+    const sessionsForUser = new Map();
+    const sessionsProcessed = [];
+  
+
+    // prepare the merge and set user map
+    reversedSessions.forEach((session) => {
+      if (options.merge && sessionsForUser.has(session.user)) {
+        const userSession = sessionsForUser.get(session.user);
+        userSession.duration += session.duration;
+        session.equipment.forEach((equipment) => {
+          userSession.equipment.add(equipment);
+        });
+      } else {
+        const clonedSession = {
+          ...session,
+          equipment: new Set(session.equipment),
+        };
+  
+        if (options.merge) {
+          sessionsForUser.set(session.user, clonedSession);
+        }
+  
+        sessionsProcessed.push(clonedSession);
+      }
+    });
+  
+    sessionsProcessed.reverse();
+  
+    const results = [];
+    const optionEquipments = new Set(options.equipment);
+    sessionsProcessed.forEach((session) => {
+      if (
+        (options.user != null && options.user !== session.user) ||
+        (optionEquipments.size > 0 &&
+          !setHasOverlap(optionEquipments, session.equipment)) ||
+        (options.minDuration != null && options.minDuration > session.duration)
+      ) {
+        return;
+      }
+  
+      results.push({
+        ...session,
+        equipment: Array.from(session.equipment).sort(),
+      });
+    });
+  
+    return results;
+  }
+  
+  // apply
+
+  // call
+  
+
+  
+
+
+ 
