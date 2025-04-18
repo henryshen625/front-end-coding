09dda7515baa86ad1525f5856dc5e698216af7c3
henry <dawangshen625@gmail.com>
2025-04-18 14:33:16 -0500

    add more 问答题

--- 问答题.js
+++ 问答题.js
@@ -186,3 +186,21 @@ function foo(p) {
   console.log(typeof b);
   let b = 0;
 }
+
+
+function Foo(x) {
+    getName = function() { console.log(1); }
+    return this;
+}
+Foo.getName = function() { console.log(2); }
+Foo.prototype.getName = function() { console.log(3); }
+var getName = function() { console.log(4); }
+function getName() { console.log(5); }
+
+Foo.getName();         // 2
+getName();             // 4
+Foo().getName();       // 1
+getName();             // 1
+new Foo().getName();   // 3
+new Foo.getName();     // 2
+new new Foo().getName(); // 3
